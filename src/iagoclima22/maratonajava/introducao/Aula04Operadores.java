c87d447c179b2e9df41b0415ad1bfa24eba984cc
Iago Lima <iagoclima22@gmail.com>
2026-06-29 17:54:01 -0300

    15 - Operadores pt 01 - Aritméticos

---
+++ src/iagoclima22/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,11 @@
+package iagoclima22.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = numero01 / numero02;
+        System.out.println(resultado);
+    }
+}
