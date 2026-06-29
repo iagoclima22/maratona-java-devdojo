fbe5746bbd7a814b733f798b9af2800e090d0e5d
Iago Lima <iagoclima22@gmail.com>
2026-06-28 23:45:56 -0300

    Aula 09 - Comentários

--- src/iagoclima22/maratonajava/introducao/OlaDevDojo.java
+++ src/iagoclima22/maratonajava/introducao/OlaDevDojo.java
@@ -1,7 +1,15 @@
 package iagoclima22.maratonajava.introducao;
-
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
 public class OlaDevDojo {
     public static void main(String[] args){
+        // Isso é um comentário de linha
+        /*
+            Isso é um comentário
+            de multiplas
+            linhas
+         */
         System.out.println("KA-ME-HA-ME-HA");
     }
 }
